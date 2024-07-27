7f63202e398e7c5afb658f9902fe7bb0150d02e3
jtran-9 <jr.tran79@gmail.com>
2024-07-26 21:39:26 -0700

    retrieved all information from build itinerary page

--- client/src/app/itinerary/page.js
+++ client/src/app/itinerary/page.js
@@ -18,6 +18,7 @@ class Event {
 export default function Itinerary() {
 
   const {start, end} = JSON.parse(window.sessionStorage.getItem('travelInfo'));
+  const destination = JSON.parse(window.sessionStorage.getItem('destination'));
 
   return (
     <m.div className={styles.itinerary}
@@ -34,7 +35,8 @@ export default function Itinerary() {
             <div>&rsaquo;</div>
           </div>
           <div className={styles.dateCard}>
-            <div>date card 2</div>
+            <div>{destination[0]}</div>
+            <div>{destination[1]}</div>
             <div>&rsaquo;</div>
           </div>
           <div className={styles.dateCard}>
@@ -51,117 +53,4 @@ export default function Itinerary() {
         </div>
     </m.div>
   );
-    /*
-
-    // CREATE A NEW Event OBJECT FOR EACH RETRIEVED FROM USER'S TRIP
-    // PLACE ALL OF THESE EVENTS IN THIS ARRAY:
-    var events = [];
-
-    // REPLACE THESE ASSIGNMENTS WITH THE RESPECTIVE DATES IN USER's TRIP
-    var first_date = new Date();
-    var final_date = new Date();
-
-    var dates = [];
-
-    dates.push(first_date);
-    if ((first_date != null) && (final_date != null)) {
-      while (first_date != final_date) {
-        first_date.setDate(first_date.getDate() + 1);
-        dates.push(first_date);
-      }
-      if (first_date != final_date) {
-        dates.push(final_date);
-      }
-    }
-
-    [useSelectedDate, setSelectedDate] = useState(new Date());
-
-    var selectedEvents = [];
-
-    function changeSelectedDate(date) {
-      setSelectedDate(date);
-
-      selectedEvents = [];
-
-      for (var i = 0 ; i < events.length ; i++) {
-        if (events[i].date == useSelectedDate) {
-          selectedEvents.push(events[i]);
-        }
-      }
-    } 
-
-    function pushNewEvent(event) {
-      selectedEvents.push(event);
-    }
-  
-    return (
-      <m.div className={styles.itinerary}
-        initial={{opacity: 0, height: 0}}
-        animate={{opacity: 1, height: "75vh"}}
-        transition={{ duration: 0.75, ease: 'easeOut' }}
-        exit={{ opacity: 1 }}
-      >
-        <div className={styles.itCol1}>
-          <div className={styles.itCol1Head}>
-            <h1>
-              Itinerary
-            </h1>
-          </div>
-
-          {dates.map((value, index) => (
-            <div className={styles.itCol1Body}
-              onClick={changeSelectedDate(value)}
-            >
-              <div className={styles.itDaySelect}>
-                {value.toString()}
-              </div>
-            </div>
-          ))}
-
-          <div className={styles.itCol1Foot}>
-            <h3>
-              {"< "} other trips
-            </h3>
-          </div>
-        </div>
-        <div className={styles.itCol2}>
-          <h1>
-            Destination
-          </h1>
-          <div className={styles.selectedDayContainer}>
-            <div className={styles.selectedDayHead}>
-              {useSelectedDate.toString()}
-            </div>
-
-            {selectedEvents.map((value, index) => (
-              <div className={styles.selectedDayBody}>
-                <div className={styles.itEvent}>
-                  <h3>
-                    {value.title}
-                  </h3>
-                  <p>
-                    {value.location}
-                  </p>
-                  <p>
-                    {value.cost}
-                  </p>
-                </div>
-              </div>
-            ))}
-
-
-            <div className={styles.addEvent}>
-              <div className={styles.addPlusButton}>
-                +
-              </div>
-              <div className={styles.addMoreText}>
-                add more events, dining, shopping, etc.
-              </div>
-            </div>
-          </div>
-        </div>
-      </m.div>
-    );
-
-    */
 }
