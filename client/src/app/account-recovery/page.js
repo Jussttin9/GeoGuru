b2c444b4299610c6eeb1bca097aa9606b788b852
jtran-9 <jr.tran79@gmail.com>
2024-08-18 20:14:13 -0700

    implemented account recovery; closes #11

--- client/src/app/account-recovery/page.js
+++ client/src/app/account-recovery/page.js
@@ -1,9 +1,13 @@
 'use client';
 
 import styles from './../page.module.css';
+import Image from 'next/image';
+import Marquee from 'react-fast-marquee';
 import { motion as m } from 'framer-motion';
 import { animate } from 'framer-motion';
 import { useState, useEffect } from 'react';
+import { auth } from '../firebase';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 // This will be our page for the Register page.
 
@@ -13,6 +17,9 @@ export default function Recovery() {
         setEmail(event.target.value)
     }
 
+    const [header, setHeader] = useState(`Sent to ${useEmail}!`);
+    const [subheader, setSubheader] = useState('Check your inbox to recover your Geoguru account.');
+
     let submitbutton = undefined;
     let submissionmsg = "";
 
@@ -21,11 +28,23 @@ export default function Recovery() {
       submissionmsg = document.getElementById("submsg");
     })
 
-    const submitrequest = () => {
-      if ((submitbutton != null)) {
-        animate(submitbutton, { backgroundColor: "#86EF98" }, {duration: 0.5})
-        animate(submissionmsg, { opacity: 1, y: "675%" }, {duration: 0.5} )
-        setTimeout(fademessage, 7500);
+    // should send a reset password email to the given email
+    const submitrequest = async () => {
+      try {
+        console.log(useEmail);
+        await sendPasswordResetEmail(auth, useEmail);
+        setHeader(`Sent to ${useEmail}!`);
+        setSubheader('Check your inbox to recover your Geoguru account.');
+      } catch (error) {
+        console.log(error);
+        setHeader('Error!');
+        setSubheader('The provided email is not linked to an account.');
+      } finally {
+        if ((submitbutton != null)) {
+          animate(submitbutton, { backgroundColor: "#86EF98" }, {duration: 0.5})
+          animate(submissionmsg, { opacity: 1, y: "625%" }, {duration: 0.5} )
+          setTimeout(fademessage, 7500);
+        }
       }
     }
 
@@ -35,18 +54,32 @@ export default function Recovery() {
 
     return (
       <>
-        <m.div className={styles.recoveryregister}
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
+        <m.div className={styles.registerFront}
+          initial={{opacity: 0, height: "0%" }}
+          animate={{opacity: 1, height: "100vh" }}
           transition={{ duration: 0.75, ease: 'easeOut' }}
           exit={{ opacity: 1 }}
         >
+          <div className={styles.bannercontainer}>
+            <div className={styles.banner}>
+              <Marquee>
+                <Image
+                    src="/TEMPgeogurubanner.svg"
+                    alt="Geoguru Full Banner [TEMPORARY]"
+                    className={styles.logoimg}
+                    width={1054}
+                    height={725}
+                    priority
+                />
+              </Marquee>
+            </div>
+          </div>
           <m.div className={styles.rrContainer}
             initial={{y: "95%"}}
-            animate={{y: "135%"}}
+            animate={{y: "-170%"}}
             transition={{ duration: 0.75, ease: 'easeOut' }}
             exit={{ opacity: 1 }}          
-          >
+            >
             <h3>Forgot your password?</h3>
             <p>Submit your email below to receive an account recovery link!</p>
             <br/>
@@ -81,19 +114,19 @@ export default function Recovery() {
                 />                
               </m.button>
             </div>
+            <m.div id="submsg" className={styles.emailsubmission}
+              initial={{opacity: 0, y: "700%"}}
+            >
+              <div>
+              <h3>
+                {header}
+              </h3>
+              <h5>
+                {subheader}
+              </h5>
+              </div>
+            </m.div>
           </m.div>
-        </m.div>
-        <m.div id="submsg" className={styles.emailsubmission}
-          initial={{opacity: 0, y: "700%"}}
-        >
-          <div>
-          <h3>
-            Sent to {useEmail}!
-          </h3>
-          <h5>
-            Check your inbox to recover your Geoguru account.
-          </h5>
-          </div>
         </m.div>
       </>
     );
--- client/src/app/navbar.js
+++ client/src/app/navbar.js
@@ -27,7 +27,7 @@ export default function Navbar() {
         setUid(localStorage.getItem('uid'));
     }, [])
 
-    if (pathname === '/' || pathname === '/register') {
+    if (pathname === '/' || pathname === '/register' || pathname === '/account-recovery') {
         return (
             <div className={styles.navbar}>
                 <Link className={styles.logo} href={"/"}>
